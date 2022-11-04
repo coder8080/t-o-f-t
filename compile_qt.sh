6ae1fd6d2732f3b6cbd472cb5963724e25432f18
Roman Kolerov <coder8080@mail.ru>
2022-11-04 20:11:44 +0300

    Добавлен скрипт компиляции окон

---
+++ compile_qt.sh
@@ -0,0 +1,6 @@
+mkdir compiled_windows
+pyuic6 ./windows/add.ui -o ./compiled_windows/add.py
+pyuic6 ./windows/files.ui -o ./compiled_windows/files.py
+pyuic6 ./windows/item.ui -o ./compiled_windows/item.py
+pyuic6 ./windows/main.ui -o ./compiled_windows/main.py
+pyuic6 ./windows/search.ui -o ./compiled_windows/search.py
