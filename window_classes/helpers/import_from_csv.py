5977f2355827e7dac6148ff66425ad711165de22
Roman Kolerov <coder8080@mail.ru>
2022-11-10 09:16:45 +0300

    Исправлена ошибка неизвестных цветов

--- window_classes/helpers/import_from_csv.py
+++ window_classes/helpers/import_from_csv.py
@@ -10,7 +10,13 @@ def import_from_csv(con, cur, filename):
         color_ids[name] = id
     query = 'INSERT INTO things (name, color_id, filename_id) VALUES '
     for line in reader:
-        query += f'("{line["название"]}", {color_ids[line["цвет"]]}, "{line["id изображения"]}"),\n'
+        color = line["цвет"]
+        if color not in color_ids:
+            create_color_query = f'INSERT INTO colors (name) VALUES ("{color}")'
+            cur.execute(create_color_query)
+            color_id = cur.execute(f'SELECT id FROM colors WHERE name = "{color}";').fetchall()[0][0]
+            color_ids[color] = color_id
+        query += f'("{line["название"]}", {color_ids[color]}, "{line["id изображения"]}"),\n'
     query = query[:-2]
     cur.execute(query)
     con.commit()
